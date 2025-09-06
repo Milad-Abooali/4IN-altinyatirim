251a29b36c4d0eac0bea90f92d58672673bc4489
Milad Abooali <m.abooali@hotmail.com>
2025-09-07 02:56:48 +0330

    feat: Tos

---
+++ qcrm/includes/tos/altinyatirim.php
@@ -0,0 +1,27 @@
+<?php
+require_once('../../config.php');
+global $_L;
+$brand ="Altin Yatirim";
+?>
+<div class="px-2">
+    <h5 style="vertical-align: inherit;"><?= $brand ?> <?= $_L->T('Legal_Documents','agreement')  ?></h5>
+    <p>
+        <?= $brand ?> <?= $_L->T('Legal_Documents_Text','agreement')  ?>
+    </p>
+    <hr>
+    <a href="https://altinyatirim.com/kullanim-kosullari/" target=" _blank" class="btn btn-sm btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16" style="margin-right: 5px;">
+            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+        </svg>
+        <?= $_L->T('Clients_Agreement','agreement')  ?>
+    </a>
+    <span class="px-3">|</span>
+    <a href="https://altinyatirim.com/gizlilik-sozlesmesi/" title="confidentiality agreement" target=" _blank" class="btn btn-sm btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16" style="margin-right: 5px;">
+            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+        </svg>
+        <?= $_L->T('Confidentiality_Agreement','agreement')  ?>
+    </a>
+</div>
